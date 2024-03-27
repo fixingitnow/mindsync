98166679b420c2cf16c4328a9bca9fb42b4a26ee
Pradeep Sai Uppula <pradeep@Pradeeps-MacBook-Pro.local>
2024-03-27 08:04:48 -0700

    add(*): support for get request and allow cors policy

--- my-worker/src/index.js
+++ my-worker/src/index.js
@@ -1,22 +1,30 @@
 const worker = {
 	async fetch(request, env, ctx) {
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+			'Access-Control-Max-Age': '86400',
+		};
 		try {
 			if (request.method === 'GET') {
-				// Parse the URL
+				// GET request: Retrieve tasks for a session
+				// Expected request parameters: URL containing sessionid
+				// Returns: Response with tasks for the session or "Session not found" if sessionid is invalid
 				const url = new URL(request.url);
-
-				// Read the sessionid from the request
 				const sessionid = url.pathname.split('/').pop();
-
 				const tasks = await env.DB1.get(sessionid);
 				if (tasks === null) {
-					return new Response('Session not found', { status: 404 });
+					return new Response('Session not found', { status: 404, headers: corsHeaders });
 				}
-				return new Response(`${tasks}`, { status: 200 });
+				console.log(tasks, 'tasks server');
+				return new Response(`${tasks}`, { status: 200, headers: corsHeaders });
 			} else if (request.method === 'POST') {
-				const { sessionid, taskname, taskdescription, taskDuration } = await request.json();
-				if (!sessionid || !taskname || !taskdescription || !taskDuration) {
-					return new Response('Invalid request', { status: 400 });
+				// POST request: Create a new task for a session
+				// Expected request parameters: JSON object containing sessionid, taskname, taskdescription, and taskduration
+				// Returns: Response indicating successful task creation or "Invalid request" if any required parameter is missing
+				const { sessionid, taskname, taskdescription, taskduration } = await request.json();
+				if (!sessionid || !taskname || !taskdescription || !taskduration) {
+					return new Response('Invalid request', { status: 400, headers: corsHeaders });
 				}
 
 				let tasks = await env.DB1.get(sessionid);
@@ -26,32 +34,38 @@ const worker = {
 					tasks = JSON.parse(tasks);
 				}
 
-				tasks.push({ taskname, taskdescription, taskDuration });
+				tasks.push({ taskname, taskdescription, taskduration });
 				await env.DB1.put(sessionid, JSON.stringify(tasks));
 
-				return new Response('Task Created', { status: 200 });
+				return new Response('Task Created', { status: 200, headers: corsHeaders });
 			} else if (request.method === 'DELETE') {
+				// DELETE request: Delete a task from a session
+				// Expected request parameters: JSON object containing sessionid and taskname
+				// Returns: Response indicating successful task deletion or "Invalid request" if any required parameter is missing,
+				// or "Session not found" if sessionid is invalid
 				const { sessionid, taskname } = await request.json();
 				if (!sessionid || !taskname) {
-					return new Response('Invalid request', { status: 400 });
+					return new Response('Invalid request', { status: 400, headers: corsHeaders });
 				}
 
 				let tasks = await env.DB1.get(sessionid);
 				if (tasks === null) {
-					return new Response('Session not found', { status: 404 });
+					return new Response('Session not found', { status: 404, headers: corsHeaders });
 				}
 
 				tasks = JSON.parse(tasks);
 				const updatedTasks = tasks.filter((task) => task.taskname !== taskname);
 				await env.DB1.put(sessionid, JSON.stringify(updatedTasks));
 
-				return new Response('Task Deleted', { status: 200 });
+				return new Response('Task Deleted', { status: 200, headers: corsHeaders });
 			} else {
-				return new Response('Method not allowed', { status: 405 });
+				// Unsupported request method
+				return new Response('Method not allowed', { status: 405, headers: corsHeaders });
 			}
 		} catch (error) {
+			// Internal server error
 			console.error(error);
-			return new Response('Internal Server Error', { status: 500 });
+			return new Response('Internal Server Error', { status: 500, headers: corsHeaders });
 		}
 	},
 };
--- src/components/TaskEntry.tsx
+++ src/components/TaskEntry.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskView from './TaskView';
+import { v4 } from 'uuid';
 
 export type Task = {
     entry: string;
     duration: string;
 };
 
+const getSessionID = () => {
+    let currentID = localStorage.getItem('sessionID')
+    let sessionID = currentID ? currentID : v4()
+    localStorage.setItem('sessionID', sessionID)
+    return sessionID
+}
+
+const serverURL = 'http://localhost:8787'
+// const serverURL = "https://my-worker.pradeepsai-aseblr.workers.dev/"
+
 const TaskEntry = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [entry, setEntry] = useState('');
     const [duration, setDuration] = useState('30');
+    const [sessionID, setSessionID] = useState('');
 
     const handleEntryChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setEntry(e.target.value);
@@ -19,13 +31,77 @@ const TaskEntry = () => {
         setDuration(e.target.value);
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
+    // Get request response from cloudflare worker
+    // [{"taskname":"testTaskName6_subtask","taskdescription":"testTaskDescription3","taskduration":"30min"},{"taskname":"testTaskName6_subtask_2","taskdescription":"testTaskDescription3","taskduration":"30min"}]
+
+    // Post request body while calling cloudflare
+    // {"sessionid":"testSessionId6", "taskname":"testTaskName6","taskdescription":"testTaskDescription3","taskduration":"30min"}
+
+    // Delete request body while calling cloudflare
+    // { "sessionid": "testSessionId6", "taskname": "testTaskName6"}
+
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         const newTask: Task = { entry, duration };
-        setTasks([newTask, ...tasks]); // Add new task to the beginning of the tasks array
-        setEntry(''); // Clear input field after submission
+        // Make HTTP request to Cloudflare Worker to store the task in KV
+        try {
+            const response = await fetch(`${serverURL}/tasks`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    sessionid: sessionID,
+                    // modify this to be unique
+                    taskname: newTask.entry,
+                    taskdescription: newTask.entry,
+                    taskduration: newTask.duration,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error('Failed to add task');
+            }
+            // Update local state with the new task
+            setTasks([newTask, ...tasks]); // Add new task to the beginning of the tasks array
+            setEntry(''); // Clear input field after submission
+        } catch (error) {
+            console.error('Error adding task:', error);
+        }
     };
 
+    useEffect(() => {
+        setSessionID(getSessionID)
+    }, [])
+
+    useEffect(() => {
+        // Fetch tasks associated with the session ID from Cloudflare KV
+        const fetchTasks = async () => {
+            try {
+                const response = await fetch(`${serverURL}/tasks/${sessionID}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch tasks');
+                }
+                const data = await response.json();
+                console.log(data, 'data client')
+                if (data && data.length > 0) {
+                    const mappedTasks = data.map((task: any) => ({
+                        entry: task.taskdescription,
+                        duration: task.taskduration,
+                    }));
+                    setTasks(mappedTasks);
+                } else {
+                    setTasks([]);
+                }
+            } catch (error) {
+                console.error('Error fetching tasks:', error);
+            }
+        };
+
+        fetchTasks();
+    }, [sessionID]);
+
+    // use supsense for fallback ui
+    console.log(sessionID, 'log')
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="bg-blue-300 p-4 rounded-md shadow-md mr-4" onSubmit={handleSubmit}>
