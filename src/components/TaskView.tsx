3d1ec8642dde7b43eb7ddbc6697fe903f1b7e032
Pradeep Sai Uppula <pradeep@Pradeeps-MacBook-Pro.local>
2024-03-29 17:03:35 -0700

    delete icon addition

--- src/components/TaskView.tsx
+++ src/components/TaskView.tsx
@@ -7,15 +7,21 @@ type TaskViewProps = {
     onDelete: Function;
 };
 
+
 const TaskView: React.FC<TaskViewProps> = ({ task, onDelete }) => {
     const { entry, duration } = task;
 
     return (
-        <div className="bg-gray-100 p-4 rounded-md shadow-md mb-4">
-            <h2 className="text-lg font-semibold mb-2">Task Details:</h2>
-            <p><strong>Task Entry:</strong> {entry}</p>
-            <p><strong>Duration:</strong> {duration} minutes</p>
-            <button onClick={() => onDelete(task)}>Delete</button>
+        <div className="relative bg-gray-100 p-4 rounded-md shadow-md mb-4">
+            <div className="flex justify-end">
+                <button onClick={() => onDelete(task)} className="absolute top-2 right-2 bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-2 w-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+            <p className="text-gray-800"><strong>Task Entry:</strong> {entry}</p>
+            <p className="text-gray-800"><strong>Duration:</strong> {duration} minutes</p>
         </div>
     );
 };
